fe559e3ef3bed76f4ef424fe320ea398b5df598b
Shanu <kumarsanu.2210@gmail.com>
2023-08-30 13:54:07 +0530

    Sign In/Sign Up Form and Login/Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./App.css";
+// import { Provider } from "react-redux";
+import Body from "./components/Body";
+// import appStore from "./utils/appStore";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-      <h1 className="">Namaste Everyone, let's build Netflix GPT</h1>
-    </div>
+    // <Provider store={appStore}>
+    <Body />
+    // </Provider>
   );
 }
 
